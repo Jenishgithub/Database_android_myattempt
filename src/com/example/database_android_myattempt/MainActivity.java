7d4a93b53f22f9a5418ca654c230ab2bd00b5af3
Jenishgithub <jenish_shakya33@yahoo.com>
2015-05-02 07:56:38 -0800

    Second commit

--- src/com/example/database_android_myattempt/MainActivity.java
+++ src/com/example/database_android_myattempt/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	Button btnDeleteDatabase;
 	RelativeLayout rlResetDatabase;
 	ProgressBar pbWwait;
-
+//yo
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
